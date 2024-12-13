18d4918b3c3af4ac1a5270aad1bc559870ce10d3
thodorisP <teosfp@hotmail.com>
2024-12-13 15:39:00 -0500

    commit

--- plot.py
+++ plot.py
@@ -1,7 +1,8 @@
+import os
 import get_data
 import get_plot
 
-path = "/home/theodoros/PROJ_ElectroCat/theodoros/HER/Au/HER_Au/slow_grow_method/tutorial_2/"
+path = os.getcwd()
 
 x, y = get_data.get_free_energy( path )
 get_plot.plot_barrier( x, y )
